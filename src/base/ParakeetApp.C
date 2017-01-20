53c62308a9aacf9fa5203983b12e1f2717cdfef9
Yunwei Mao <yunweimao@dhcp-18-111-34-176.dyn.mit.edu>
2017-01-20 09:38:07 -0500

    Start to finish

---
+++ src/base/ParakeetApp.C
@@ -0,0 +1,50 @@
+#include "ParakeetApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+template<>
+InputParameters validParams<ParakeetApp>()
+{
+  InputParameters params = validParams<MooseApp>();
+  return params;
+}
+
+ParakeetApp::ParakeetApp(InputParameters parameters) :
+    MooseApp(parameters)
+{
+  Moose::registerObjects(_factory);
+  ModulesApp::registerObjects(_factory);
+  ParakeetApp::registerObjects(_factory);
+
+  Moose::associateSyntax(_syntax, _action_factory);
+  ModulesApp::associateSyntax(_syntax, _action_factory);
+  ParakeetApp::associateSyntax(_syntax, _action_factory);
+}
+
+ParakeetApp::~ParakeetApp()
+{
+}
+
+// External entry point for dynamic application loading
+extern "C" void ParakeetApp__registerApps() { ParakeetApp::registerApps(); }
+void
+ParakeetApp::registerApps()
+{
+  registerApp(ParakeetApp);
+}
+
+// External entry point for dynamic object registration
+extern "C" void ParakeetApp__registerObjects(Factory & factory) { ParakeetApp::registerObjects(factory); }
+void
+ParakeetApp::registerObjects(Factory & factory)
+{
+}
+
+// External entry point for dynamic syntax association
+extern "C" void ParakeetApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory) { ParakeetApp::associateSyntax(syntax, action_factory); }
+void
+ParakeetApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
+{
+}
