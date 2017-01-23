7aedcab312f58255e1104ecb5335d754532f2a5b
Yunwei Mao <yunweimao@dhcp-18-111-126-123.dyn.MIT.EDU>
2017-01-23 12:50:46 -0500

    Finish the damage part

--- src/base/ParakeetApp.C
+++ src/base/ParakeetApp.C
@@ -4,6 +4,28 @@
 #include "ModulesApp.h"
 #include "MooseSyntax.h"
 
+//add include files for pure deformation problem.
+#include "DeformationParakeetAction.h"
+
+#include "DisplacementParakeet.h"
+#include "VelocityParakeet.h"
+#include "PressureParakeet.h"
+
+#include "StressDivergenceParakeet.h"
+
+#include "ComputeFiniteStrainParakeet.h"
+#include "ComputeArrudaBoyceMPParakeet.h"
+#include "ArrudaBoyceParakeet.h"
+
+//add include files for deformation+damage problem.
+// strain part is the same.
+#include "ComputeDamageParakeet.h"
+#include "ComputeArrudaBoyceDamageMPParakeet.h"
+#include "ArrudaBoyceDamagePart1Parakeet.h"
+#include "ArrudaBoyceDamagePart2Parakeet.h"
+#include "DamageParakeet.h"
+//#include "DamageParakeetAction.h"
+
 template<>
 InputParameters validParams<ParakeetApp>()
 {
@@ -40,11 +62,38 @@ extern "C" void ParakeetApp__registerObjects(Factory & factory) { ParakeetApp::r
 void
 ParakeetApp::registerObjects(Factory & factory)
 {
+
+      //kernels
+      registerKernel(StressDivergenceParakeet);
+
+      //materials
+      registerMaterial(ComputeFiniteStrainParakeet);
+      registerMaterial(ComputeArrudaBoyceMPParakeet);
+      registerMaterial(ArrudaBoyceParakeet);
+
+      //BCs
+      registerBoundaryCondition(VelocityParakeet);
+      registerBoundaryCondition(PressureParakeet);
+      registerBoundaryCondition(DisplacementParakeet);
+
+      // for damage 
+      registerKernel(DamageParakeet);
+      registerMaterial(ComputeDamageParakeet);
+      registerMaterial(ComputeArrudaBoyceDamageMPParakeet);
+      registerMaterial(ArrudaBoyceDamagePart1Parakeet);
+      registerMaterial(ArrudaBoyceDamagePart2Parakeet);
+
 }
 
 // External entry point for dynamic syntax association
 extern "C" void ParakeetApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory) { ParakeetApp::associateSyntax(syntax, action_factory); }
 void
-ParakeetApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
+ParakeetApp::associateSyntax(Syntax & syntax, ActionFactory & action_factory)
 {
+
+     syntax.registerActionSyntax("DeformationParakeetAction", "Kernels/DeformationParakeet");
+     registerAction(DeformationParakeetAction, "add_kernel");
+     //syntax.registerActionSyntax("DamageParakeetAction", "Kernels/DamageParakeet");
+     //registerAction(DamageParakeetAction, "add_kernel");
+
 }
